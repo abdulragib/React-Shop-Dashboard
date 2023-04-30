052356477121264c26754bd5b1829d58847c946d
Abdul Ragib <abdulragib36@gmail.com>
2023-04-30 14:21:11 +0530

    Sales , Revenue, Expenses card done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css'
+import Maindash from './components/MainDash/Maindash';
 import Sidebar from './components/SideBar/Sidebar'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
         <div className='AppGlass'>
            <Sidebar/>
+           <Maindash/>
         </div>
     </div>
   );
---
+++ src/components/MainDash/Maindash.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Maindash.css'
+import Cards from '../Cards/Cards'
+
+const Maindash = () => {
+  return (
+    <div className='MainDash'>
+        <h1>Dashboard</h1>
+        <Cards/>
+    </div>
+  )
+}
+
+export default Maindash
