40cd79c0bd0c8bfed013d880cee4460e54ae32d8
Abdul Ragib <abdulragib36@gmail.com>
2023-05-01 14:07:39 +0530

    SideBar responsiveness done

--- src/components/SideBar/Sidebar.jsx
+++ src/components/SideBar/Sidebar.jsx
@@ -2,15 +2,33 @@ import React, { useState } from "react";
 import "./Sidebar.css";
 import Logo from "../../imgs/logo.png";
 import { UilEstate } from "@iconscout/react-unicons";
-import { UilSignOutAlt } from "@iconscout/react-unicons";
+import { UilSignOutAlt,UilBars } from "@iconscout/react-unicons";
 
 import { SidebarData } from "../../Data/Data";
+import {motion} from 'framer-motion'
 
 const Sidebar = () => {
   const [selected, setSelected] = useState(0);
+  const [expanded, setExpanded]=useState(true);
+
+  const sidebarVariants={
+    true:{
+      left:'0',
+    },
+
+    false:{
+      left:'-60%'
+    },
+  }
 
   return (
-    <div className="Sidebar">
+    <>
+    {/* toggle button */}
+    <div className="bars" style={expanded?{left:'60%'}:{left:'5%'}} onClick={()=>setExpanded(!expanded)}>
+        <UilBars/>
+    </div>
+
+    <motion.div className="Sidebar" variants={sidebarVariants} animate={window.innerWidth<=768?`${expanded}`:''}>
       {/* logo */}
       <div className="logo">
         <img src={Logo} alt="" />
@@ -38,7 +56,8 @@ const Sidebar = () => {
           <UilSignOutAlt />
         </div>
       </div>
-    </div>
+    </motion.div>
+    </>
   );
 };
 
